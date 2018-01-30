0615c2b61b236f638e4ced34d6e4b3ae770bf937
miamiworldwide <rivendalejones@gmail.com>
2018-01-30 14:37:42 -0500

    finishing week 2 example

--- 2Week/DWDapp2.js
+++ 2Week/DWDapp2.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+app.use(express.static('public'));
+
+var count = 0;
 
 //when someone makes a get request for /
 //respond with "hello world"
@@ -7,10 +10,32 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+
+var theseSubmissions = [];
+
+app.get('/formpost', function(req,res){
+  res.send("you submitted "+ req.query.textfield);
+  theseSubmissions.push(req.query.textfield);
+});
+
+app.get('/display', function(req,res){
+  let htmlout = "<html><body>";
+  for(var i = 0; i< theseSubmissions.length; i++){
+    htmlout = htmlout +theseSubmissions[i] + "<br>";
+  }
+  htmlout = htmlout + "</body></html>";
+  res.send(htmlout);
+})
+
 app.get('/somethingelse', function (req, res) {
   res.send('goodbye')
-})
+});
+
+app.get('/count', function (req, res) {
+  count++;
+  res.send('<html><body>'+count+'</body></html>')
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
--- 2Week/notes.md
+++ 2Week/notes.md
@@ -1,2 +1,8 @@
 ## JS libraries
 * D3, paper.js, three.js
+
+## server history
+* norm used to be big multi purpose Apache servers (dreamhost, bluehost)
+* now small single purpose servers are the norm (virtual private servers in cloud)
+* now lets you run your own operating system and your own software
+* linux virtualization
