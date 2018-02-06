217468d05de21cb1be4ca5abd61d14f69a0d7f88
miamiworldwide <rivendalejones@gmail.com>
2018-02-06 01:05:47 -0500

    adding week 2 homework first draft

---
+++ 2Week/assignment/DWDAssignment2.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+app.use(express.static('public'));
+
+var count = 0;
+
+//when someone makes a get request for /
+//respond with "hello world"
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
---
+++ 2Week/assignment/public/sketch.js
@@ -0,0 +1,98 @@
+var nameData = JSON.parse(localStorage.nameData || null) || {};
+var socket = io.connect(window.location.origin);
+var tableNames;
+var palms;
+var song;
+var showImage = false;
+
+function preload(){
+  song = loadSound('/assets/stangetz.mp3');
+}
+
+function setup(){
+  createCanvas(400, 400);
+  input = createInput();
+  input.position(20, 100);
+  button = createButton('submit');
+  button.position(input.x + input.width, 100);
+  button.mousePressed(greet);
+  greeting = createElement('h2', 'it feels better out here, away from the party. may I ask your name?');
+  greeting.position(20, 5);
+  textAlign(CENTER);
+  textSize(50);
+  song.loop();
+  palms = document.getElementById("palms");
+}
+
+function draw(){
+  // background(255);
+  // text(loadStuff(), width/2,height/2);
+  if(showImage){
+    palms.hide();
+  }
+}
+
+function greet() {
+  var name = input.value();
+  greeting.html('it\'s a pleasure, '+name+'. it really is');
+  input.value('');
+  saveStuff(name);
+  button.remove();
+  nextButton = createButton("submit");
+  nextButton.position(input.x + input.width, 100);
+  nextButton.mousePressed(journey);
+}
+
+function journey(){
+  var newText = input.value();
+  greeting.html("still here?");
+  input.value('');
+  saveStuff(newText);
+  nextButton.remove();
+  unholyButton = createButton("clikc please");
+  unholyButton.position(input.x + input.width, 65);
+  unholyButton.mousePressed(showAll);
+}
+
+function showAll(){
+  unholyButton.remove();
+  greeting.remove();
+  input.remove();
+  _dataLoader("names.csv");
+  lastButton = createButton("see friends");
+  lastButton.position(10,10);
+  lastButton.mousePressed(printOut);
+}
+
+function printOut(){
+  lastButton.remove();
+  console.log(tableNames.getColumn("name"));
+  for(var r = 0; r < tableNames.getRowCount(); r++){
+    textSize(10);
+    text(tableNames.getRow(r).get(0),width/2,10+10*r);
+  }
+}
+
+function _dataLoader(dataBaseName,){
+  tableNames = loadTable(dataBaseName,"csv","header");
+  console.log("data loaded");
+  console.log(tableNames.getColumn("name"));
+}
+
+
+// Store your data.
+function saveStuff(obj) {
+  nameData.name = obj;
+  nameData.time = new Date().getTime();
+  // localStorage.nameData = JSON.stringify(nameData);
+  _emitter(nameData);
+}
+
+function loadStuff() {
+  return nameData.name || "default";
+}
+
+function _emitter(data){
+  socket.emit('nameSocket', data);
+  //console.log(data);
+}
